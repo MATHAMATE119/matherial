61454cbb2d116211cdf2cfd4161d3b90edc32f38
MATHAMATE119 <jerzish@yandex.ru>
2022-01-27 02:25:09 +0300

    QEREQEQRQERQE

--- 1.c
+++ 1.c
@@ -1,1 +1,5 @@
 check the method by lord finesse in instrumental version is polydope track
+]
+choose the sword and u will join me
+choose the ball and u join ur mother in death
+u dont understand my words but u must choose 
